d2fa677672b6d8367ab4af77c864e7432df39cf5
bcbroookman <bcbrookman@users.noreply.github.com>
2019-02-17 17:05:42 -0800

    Add database models for initial migration

--- search/models.py
+++ search/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Image (models.Model):
+    partNumber = models.TextField(primary_key=True)
+    image_path = models.TextField(null=True)
+
+
+class Device (models.Model):
+    serialNumber = models.TextField(primary_key=True)
+    mac = models.TextField()
+    apGroupName = models.TextField(null=True)
+    deviceDescription = models.TextField(null=True)
+    deviceFullName = models.TextField(null=True)
+    deviceName = models.TextField(null=True)
+    firstSeen = models.TextField(null=True)
+    folder = models.TextField(null=True)
+    folderId = models.TextField(null=True)
+    inventoryDate = models.TextField(null=True)
+    lastAosVersion = models.TextField(null=True)
+    lastBootVersion = models.TextField(null=True)
+    lastSeen = models.TextField(null=True)
+    partCategory = models.TextField(null=True)
+    partNumber = models.ForeignKey(Image, on_delete=models.CASCADE)
+    sourceIpAddress = models.TextField(null=True)
+    status = models.TextField(null=True)
