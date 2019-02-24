06b748e2e9040fb83d7c2884fc137d8aa5c55c17
bcbroookman <bcbrookman@users.noreply.github.com>
2019-02-24 13:56:07 -0800

    Add web content into templates

--- search/views.py
+++ search/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the search index.")
+    return render(request, 'search/search.html')
+
+
+def about(request):
+    return render(request, "search/about.html")
