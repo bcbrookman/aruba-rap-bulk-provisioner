4ad785e2625d8dc09bb41c6dc7a8365c49dbbd3c
bcbrookman <bcbrookman@users.noreply.github.com>
2019-03-02 11:32:50 -0800

    Move API search function to independent file

--- search/urls.py
+++ search/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('search', views.index, name='index'),
-    path('about', views.about, name='about'),
-    path('api/search/<query>', views.api_search, name='search')
+    path('', views.search_index, name='index'),
+    path('search', views.search_index, name='index'),
+    path('about', views.search_about, name='about'),
+    path('api/search/<query>', views.api_search, name='search'),
 ]
--- search/views.py
+++ search/views.py
@@ -1,78 +1,16 @@
 from django.shortcuts import render
-from . models import Device, Image
-from django.db.models import Q
 from django.http import JsonResponse
+from . api_search import api_search as search
 
 
-def index(request):
+def search_index(request):
     return render(request, 'search/search.html')
 
 
-def about(request):
+def search_about(request):
     return render(request, "search/about.html")
 
 
 def api_search(request, query):
-    result = {
-        'devices': []
-    }
-
-    query = query.strip()
-    resp = Device.objects.select_related('image_path').filter(Q(deviceFullName__icontains=query) |
-                                                              Q(mac__icontains=query) |
-                                                              Q(serialNumber__icontains=query) |
-                                                              Q(deviceDescription__icontains=query) |
-                                                              Q(deviceName__icontains=query)
-                                                              ).values('apGroupName',
-                                                                       'deviceDescription',
-                                                                       'deviceFullName',
-                                                                       'deviceName',
-                                                                       'folder',
-                                                                       'folderId',
-                                                                       'partNumber__image_path',
-                                                                       'partNumber',
-                                                                       'lastAosVersion',
-                                                                       'lastBootVersion',
-                                                                       'lastSeen',
-                                                                       'partCategory',
-                                                                       'sourceIpAddress',
-                                                                       'firstSeen',
-                                                                       'inventoryDate',
-                                                                       'mac',
-                                                                       'serialNumber',
-                                                                       'status',
-                                                                       )
-
-    for r in resp:
-        device = {
-            'additionalData': {
-                'apGroupName': r['apGroupName'],
-                'deviceDescription': r['deviceDescription'],
-                'deviceFullName': r['deviceFullName'],
-                'deviceName': r['deviceName'],
-                'firstSeen': r['firstSeen'],
-                'folder': r['folder'],
-                'folderId': r['folderId'],
-                'img': r['partNumber__image_path'],
-                'lastAosVersion': r['lastAosVersion'],
-                'lastBootVersion': r['lastBootVersion'],
-                'lastSeen': r['lastSeen'],
-                'partCategory': r['partCategory'],
-                'sourceIpAddress': r['sourceIpAddress'],
-            },
-            'firstSeen': r['firstSeen'],
-            'folderId': r['folderId'],
-            'inventoryDate': r['inventoryDate'],
-            'lastSeen': r['lastSeen'],
-            'mac': r['mac'],
-            'serialNumber': r['serialNumber'],
-            'partNumber': r['partNumber'],
-            'status': r['status'],
-        }
-        result['devices'].append(device)
-
-    # If there are no results, return Not Found error instead
-    if len(result['devices']) == 0:
-        result = {'error': "Not Found"}
-
-    return JsonResponse(result)
+    q = str(query)
+    return JsonResponse(search(q))
